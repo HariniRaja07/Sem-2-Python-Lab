a9bd8de06c11e37a5c3f1b808702e584d0d47237
HariniRaja07 <harinibharathraj07@gmail.com>
2024-11-28 14:47:57 +0530

    Add files via upload

---
+++ python lab 3.py
@@ -0,0 +1,15 @@
+length=int(input("Enter length:"))
+breath=int(input("Enter breath:"))
+area_rectangle=length*breath
+print(f"The Area of the Rectangle:{area_rectangle} square units")
+a=int(input("Enter side:"))
+area_square=a*a
+print(f"The Area of the square:{area_square} square units")
+base=int(input("Enter base:"))
+height=int(input("Enter height:"))
+area_triangle=1/2*base*height
+print(f"The Area of the triangle:{area_triangle} square units")
+radius=int(input("Enter radius:"))
+area_circle=3.14*(radius*radius)
+print(f"The Area of the Circle:{area_circle} square units")
+
